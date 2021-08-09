fdb887bdb45902e37526ee606ab69bab3c62008d
Eliu Hernandez <3liuhernandez@gmail.com>
2021-08-09 12:22:27 -0400

    ajustes layout y login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,22 +1,70 @@
-<?php $this->load->view("layout/header"); ?>
+<!DOCTYPE html>
+<html lang="es" prefix="og: https://ogp.me/ns#">
+<head prefix="home: <?php echo base_url()?>home">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Primary Meta Tags -->
+    <title>Proyecto - Principe de paz</title>
+    <meta name="title" content="Proyecto - Principe de paz">
+    <meta name="description" content="Proyecto - Principe de paz">
 
 
-<div class="container">
-    <h2>Formulario login</h2>
-    <form action="/action_page.php">
-    <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('lib/css/bs/bootstrap.min.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('lib/css/login.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('lib/css/loader_css.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('lib/css/animate.css')?>" />
+    <!-- fonts -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('lib/fontawesome/css/fontawesome.min.css') ?>">
+</head>
+<body>
+    <div class="container d-flex flex-column">
+        <form action="/action_page.php" class="d-flex flex-column align-items-center">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd" name="pswd" placeholder="Enter password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-    <div class="form-group">
-        <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+
+    <div id="modal_spinner" class="container-fluid bg-dark">
+        <strong class="text-white mt-2 w3-center">
+            <b class="fa fa-spinner fa-spin"></b>
+        </strong>
+        <p class="text-white mt-4">
+            [Si la pantalla persiste por más de 3 minutos: recargue la página]
+        </p>
     </div>
-    <div class="form-group form-check">
-        <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" name="remember"> Remember me
-        </label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+
+
+    <script src="<?php echo base_url('lib/js/app.js') ?>"></script>
+    <script src="<?php echo base_url('lib/js/sweetalert.min.js') ?>"></script>
+    <script src="<?php echo base_url('lib/js/jquery-3.2.1.slim.min.js') ?>"></script>
+    <script src="<?php echo base_url('lib/js/popper.min.js') ?>"></script>
+    <script src="<?php echo base_url('lib/js/jquery-validate.min.js') ?>"></script>
+
+    <script>
+		$(document).ready(() => {
+            showSpiner();
+            setTimeout(() => {
+                hideSpiner();
+            }, 5000);
+			$("#form_register").validate();
+
+			$("#submit_form").click((e) => {
+				e.preventDefault();
+				let validado = $("#form_register").valid();
+				if(validado){
+					guardarDatos();
+				}
+				return false;
+			});
+		});
+	</script>
+
+</body>
+</html>
